6d12c350ef0f78cdc3db61d6fc0bb79d146d9bb1
くにくに <kunitatsu427@gmail.com>
2021-10-09 13:44:05 +0900

    secondeView 仮作成

---
+++ iOSCoffeeTime/iOSCoffeeTime/SecondeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondeViewController.swift
+//  iOSCoffeeTime
+//
+//  Created by 崎原邦達 on 2021/10/09.
+//
+
+import UIKit
+
+
+class SecondeViewController: UIViewController, StoryBoardInstiatable {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
